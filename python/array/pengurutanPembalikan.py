2d38fa82053fcccf8e6bf38934b65e2ed14daade
Adistian Herlambang <adistian59@gmail.com>
2026-01-18 15:23:49 +0700

    pengurutan dan pembalikan

---
+++ python/array/pengurutanPembalikan.py
@@ -0,0 +1,13 @@
+arr = [3, 1, 4, 2]
+
+# sort (in place)
+arr.sort()              # [1, 2, 3, 4]
+
+# sort descending
+arr.sort(reverse=True) # [4, 3, 2, 1]
+
+# sorted (tidak ubah array asli)
+new_arr = sorted(arr)
+
+# reverse (in place)
+arr.reverse()           # [1, 2, 3, 4]
